f98fe28c7c6f55e1b6e44bb14a417e126fd4b684
ajaysingh-01 <at0185804@gmail.com>
2025-02-18 17:30:09 +0530

    Add files via upload

---
+++ stopwatches/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import './index.css';
+import Stopwatch from './Components/Stopwatch';
+
+function App() {
+  return (
+    <>
+    <Stopwatch/>
+    </>
+  );
+}
+export default App;
